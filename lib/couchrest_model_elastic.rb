1b63fe19fa9d620801a2ef899c89d8f323f31759
awilliams <adam@cabify.com>
2013-11-13 13:20:18 +0100

    adds OpenStruct#to_h method for older rubies

--- lib/couchrest_model_elastic.rb
+++ lib/couchrest_model_elastic.rb
@@ -2,7 +2,15 @@ require 'couchrest_model_elastic/version'
 require 'ostruct'
 
 module CouchrestModelElastic
-  Config = OpenStruct.new(:hosts => [{:host => 'localhost', :port => 9200}], :log => false, :trace => false)
+  class ConfigStruct < OpenStruct
+    # Backport OpenStruct#to_h from 2.0.0
+    unless OpenStruct.public_methods.include?(:to_h)
+      def to_h
+        @table.dup
+      end
+    end
+  end
+  Config = ConfigStruct.new(:hosts => [{:host => 'localhost', :port => 9200}], :log => false, :trace => false)
 
   def self.config(&config)
     Config.tap(&config)
